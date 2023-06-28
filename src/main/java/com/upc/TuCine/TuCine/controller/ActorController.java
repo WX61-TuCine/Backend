04892504340444efde1cf707471368854fc17345
Axel Joshue Fiestas Santisteban <josue123452002@gmail.com>
2023-06-28 10:10:07 -0500

    feat: Documentation Actor and Business Controller

--- src/main/java/com/upc/TuCine/TuCine/controller/ActorController.java
+++ src/main/java/com/upc/TuCine/TuCine/controller/ActorController.java
@@ -3,6 +3,11 @@ package com.upc.TuCine.TuCine.controller;
 import com.upc.TuCine.TuCine.dto.ActorDto;
 import com.upc.TuCine.TuCine.dto.save.Actor.ActorSaveDto;
 import com.upc.TuCine.TuCine.service.ActorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -28,6 +33,14 @@ public class ActorController {
     //Method: GET
     @Transactional(readOnly = true)
     @GetMapping("/actors")
+    @Operation(summary = "Obtener lista de todos los actores")
+    @ApiResponses(value={
+            @ApiResponse(responseCode = "200", description = "Se obtuvo la lista de actores",
+            content = {
+                    @Content(mediaType = "application/json",
+                    schema= @Schema(implementation = ActorDto.class))
+            })
+    })
     public ResponseEntity<List<ActorDto>> getAllActors() {
         return new ResponseEntity<List<ActorDto>>(actorService.getAllActors(), HttpStatus.OK);
     }
@@ -36,6 +49,18 @@ public class ActorController {
     //Method: POST
     @Transactional
     @PostMapping("/actors")
+    @Operation(summary = "Crear un actor")
+    @ApiResponses(value={
+            @ApiResponse(responseCode = "201", description = "Se creó el actor",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema= @Schema(implementation = ActorDto.class))
+                    }),
+            @ApiResponse(responseCode = "400", description = "El actor no se pudo crear",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Error interno del servidor",
+                    content = @Content)
+    })
     public ResponseEntity<ActorDto> createActor(@RequestBody ActorSaveDto actorSaveDto){
         return new ResponseEntity<ActorDto>(actorService.createActor(actorSaveDto), HttpStatus.CREATED);
     }
--- src/main/java/com/upc/TuCine/TuCine/controller/BusinessController.java
+++ src/main/java/com/upc/TuCine/TuCine/controller/BusinessController.java
@@ -1,10 +1,16 @@
 package com.upc.TuCine.TuCine.controller;
 
+import com.upc.TuCine.TuCine.dto.ActorDto;
 import com.upc.TuCine.TuCine.dto.BusinessDto;
 import com.upc.TuCine.TuCine.dto.BusinessTypeDto;
 import com.upc.TuCine.TuCine.dto.ShowtimeDto;
 import com.upc.TuCine.TuCine.dto.save.Business.BusinessSaveDto;
 import com.upc.TuCine.TuCine.service.BusinessService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -30,6 +36,14 @@ public class BusinessController {
     //Method: GET
     @Transactional(readOnly = true)
     @GetMapping("/businesses")
+    @Operation(summary = "Obtener lista de todos los Businesses")
+    @ApiResponses(value={
+            @ApiResponse(responseCode = "200", description = "Se obtuvo la lista de businesses",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema= @Schema(implementation = BusinessDto.class))
+                    }),
+    })
     public ResponseEntity<List<BusinessDto>> getAllBusinesses() {
         return new ResponseEntity<>(businessService.getAllBusiness(), HttpStatus.OK);
     }
@@ -38,6 +52,16 @@ public class BusinessController {
     //Method: GET
     @Transactional(readOnly = true)
     @GetMapping("/businesses/{id}")
+    @Operation(summary = "Obtener un business por su id")
+    @ApiResponses(value={
+            @ApiResponse(responseCode = "200", description = "Se obtuvo el business",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema= @Schema(implementation = BusinessDto.class))
+                    }),
+            @ApiResponse(responseCode = "404", description = "No se encontro el business",
+                    content = @Content)
+    })
     public ResponseEntity<BusinessDto> getBusinessById(@PathVariable(value = "id") Integer id) {
         BusinessDto businessDto = businessService.getBusinessById(id);
         if (businessDto == null) {
@@ -50,6 +74,16 @@ public class BusinessController {
     //Method: GET
     @Transactional(readOnly = true)
     @GetMapping("/businesses/{id}/businessTypes")
+    @Operation(summary = "Obtener un businessType mediante el id del Business")
+    @ApiResponses(value={
+            @ApiResponse(responseCode = "200", description = "Se obtuvo el businessType",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema= @Schema(implementation = BusinessTypeDto.class))
+                    }),
+            @ApiResponse(responseCode = "404", description = "No se encontro el business",
+                    content = @Content)
+    })
     public ResponseEntity<BusinessTypeDto> getBusinessTypeByBusinessId(@PathVariable("id") Integer id) {
         BusinessTypeDto businessTypeDto = businessService.getBusinessTypeByBusinessId(id);
         if (businessTypeDto == null) {
@@ -62,6 +96,16 @@ public class BusinessController {
     //Method: GET
     @Transactional(readOnly = true)
     @GetMapping("/businesses/{id}/showtimes")
+    @Operation(summary = "Obtener todos los showtimes mediante el id del Business")
+    @ApiResponses(value={
+            @ApiResponse(responseCode = "200", description = "Se obtuvo la lista de showtimes",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema= @Schema(implementation = ShowtimeDto.class))
+                    }),
+            @ApiResponse(responseCode = "404", description = "No se encontro el business",
+                    content = @Content)
+    })
     public ResponseEntity<List<ShowtimeDto>> getAllShowtimesByBusinessId(@PathVariable("id") Integer id) {
         List<ShowtimeDto> showtimeDtoList = businessService.getAllShowtimesByBusinessId(id);
         if (showtimeDtoList == null) {
@@ -74,6 +118,16 @@ public class BusinessController {
     //Method: POST
     @Transactional
     @PostMapping("/businesses")
+    @Operation(summary = "Crear un business")
+    @ApiResponses(value={
+            @ApiResponse(responseCode = "201", description = "Se creo el business",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema= @Schema(implementation = BusinessDto.class))
+                    }),
+            @ApiResponse(responseCode = "400", description = "No se pudo crear el business",
+                    content = @Content)
+    })
     public ResponseEntity<BusinessDto> createBusiness(@RequestBody BusinessSaveDto businessSaveDto){
         return new ResponseEntity<>(businessService.createBusiness(businessSaveDto), HttpStatus.CREATED);
     }
