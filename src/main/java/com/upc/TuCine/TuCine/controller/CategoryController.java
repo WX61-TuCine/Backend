a181052f5ac9a51193f2087b15155e27b0d0202c
Axel Joshue Fiestas Santisteban <josue123452002@gmail.com>
2023-06-28 10:31:32 -0500

    feat: CategoryController documented

--- src/main/java/com/upc/TuCine/TuCine/controller/CategoryController.java
+++ src/main/java/com/upc/TuCine/TuCine/controller/CategoryController.java
@@ -1,9 +1,15 @@
 package com.upc.TuCine.TuCine.controller;
 
 
+import com.upc.TuCine.TuCine.dto.BusinessTypeDto;
 import com.upc.TuCine.TuCine.dto.CategoryDto;
 import com.upc.TuCine.TuCine.dto.save.Category.CategorySaveDto;
 import com.upc.TuCine.TuCine.service.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -31,6 +37,16 @@ public class CategoryController {
     //Method: GET
     @Transactional(readOnly = true)
     @GetMapping("/categories")
+    @Operation(summary = "Obtener lista de todas las categorias")
+    @ApiResponses(value={
+            @ApiResponse(responseCode = "200", description = "Se obtuvo la lista de categorias",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema= @Schema(implementation = BusinessTypeDto.class))
+                    }),
+            @ApiResponse(responseCode = "400", description = "No se pudo obtener la lista",
+                    content = @Content)
+    })
     public ResponseEntity<List<CategoryDto>> getAllCategories() {
         return new ResponseEntity<List<CategoryDto>>(categoryService.getAllCategories(), HttpStatus.OK);
     }
@@ -39,6 +55,16 @@ public class CategoryController {
     //Method: POST
     @Transactional
     @PostMapping("/categories")
+    @Operation(summary = "Crear una categoria de película")
+    @ApiResponses(value={
+            @ApiResponse(responseCode = "201", description = "Se creó la categoria",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema= @Schema(implementation = CategoryDto.class))
+                    }),
+            @ApiResponse(responseCode = "400", description = "La categoria no se pudo crear",
+                    content = @Content)
+    })
     public ResponseEntity<CategoryDto> createCategory(@RequestBody CategorySaveDto categorySaveDto){
         return new ResponseEntity<>(categoryService.createCategory(categorySaveDto), HttpStatus.CREATED);
     }
