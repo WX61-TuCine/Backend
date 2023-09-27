6a37a4df516fb4eb4f412e38dc81ed79dbc456e9
Tania Vásquez <103429138+taniasadith@users.noreply.github.com>
2023-09-27 02:30:43 -0500

    fix: type user

--- src/main/java/com/upc/TuCine/TuCine/repository/TypeUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.upc.TuCine.TuCine.repository;
-
-import com.upc.TuCine.TuCine.model.TypeUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TypeUserRepository extends JpaRepository<TypeUser,Integer> {
-    boolean existsTypeUserByName(String name);
-
-}
